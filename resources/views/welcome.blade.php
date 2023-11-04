64a8c0144e766eef76f423782d718d5cb8c84d69
CarlitosFigueiredo <medeirosfigueiredoc@gmail.com>
2023-11-03 21:34:55 -0300

    fim

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,13 @@
 <x-layout>
 
-    <section class="container mx-auto px-6 h-full flex flex-col">
+    <section class="container mx-auto px-6 h-full flex flex-col" style="max-width: 1000px" >
 
         <div class="container mx-auto">
             
-            <header class="flex justify-center print:justify-start">
+            <header class="flex">
 
                 <h1>
-                    <img src="/logo.svg" alt="CodeBreaker" aria-label="Codebreaker" class="print:invert print:w-36">
+                    <img src="/logo.svg" alt="CodeBreaker" aria-label="Codebreaker" class="print:invert w-52 print:w-36">
                 </h1>
             </header>
 
